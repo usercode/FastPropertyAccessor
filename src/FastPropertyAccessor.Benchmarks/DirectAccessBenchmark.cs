6c46f9d43ed571222e5c0932251b4f5ce08cdc52
usercode <2958488+usercode@users.noreply.github.com>
2022-09-09 14:46:57 +0200

    Benchmark

--- src/FastPropertyAccessor.Benchmarks/DirectAccessBenchmark.cs
+++ src/FastPropertyAccessor.Benchmarks/DirectAccessBenchmark.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using FastMember;
 using System.Reflection;
 
 namespace FastPropertyAccessor.Benchmarks
@@ -12,6 +13,7 @@ namespace FastPropertyAccessor.Benchmarks
         private Action<Model> _delegateCall;
         private Model _model;
         private FastMember.TypeAccessor _fastMember;
+        private Member _fastMemberProperty;
 
         public DirectAccessBenchmark()
         {
@@ -21,46 +23,45 @@ namespace FastPropertyAccessor.Benchmarks
             _accessor = PropertyAccessor.Get(_propertyInfo);
             _delegateCall = x => x.Value = 101;
             _fastMember = FastMember.TypeAccessor.Create(_model.GetType());
-
         }
 
-        [Benchmark(Baseline = true)]
+        [Benchmark(Baseline = true, Description = "DirectCall")]
         public void UseDirectCall()
         {
             _model.Value = 101;
         }
 
-        [Benchmark]
+        [Benchmark(Description = "DelegateCall")]
         public void UseDelegateCall()
         {
             _delegateCall(_model);
         }
 
-        [Benchmark]
-        public void UsePropertyAccessorWithBoxing()
-        {
-            _accessor.SetValue(_model, 101);
-        }
-
-        [Benchmark]
-        public void UsePropertyAccessor()
+        [Benchmark(Description = "PropertyAccessorNoBoxing")]
+        public void UsePropertyAccessorNoBoxing()
         {
             _accessor.SetInt32Value(_model, 101);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "PropertyAccessor")]
+        public void UsePropertyAccessorBoxing()
+        {
+            _accessor.SetValue(_model, 101);
+        }
+
+        [Benchmark(Description = "TypeAccessor")]
         public void UseTypeAccessor()
         {
             _typeAccessor[_model, nameof(Model.Value)] = 101;
         }
 
-        [Benchmark]
+        [Benchmark(Description = "FastMemberTypeAccessor")]
         public void UseFastMember()
         {
             _fastMember[_model, nameof(Model.Value)] = 101;
         }
 
-        [Benchmark]
+        [Benchmark(Description = "Reflection")]
         public void UseReflection()
         {
             _propertyInfo.SetValue(_model, 101);
